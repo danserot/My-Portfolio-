eecffcc96387e42187374687507cb9c09d3d4fb2
Dancerot <tema324756@gmail.com>
2026-03-03 21:15:41 +0300

    Final version 2.0

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/header.css";
 
@@ -7,45 +7,64 @@ export default function Header() {
   const toggleMenu = () => {
     setIsOpen((prev) => !prev);
   };
-  return (
-    <div className="header">
-      <div className="headerblock">
-        <a href="Index.html">
-          <img
-            className="header_logo"
-            src="Images/sign-removebg-preview.png"
-            alt="Header_logo"
-          />
-        </a>
-      </div>
-      <nav
-        className={`hamburger ${isOpen ? "active" : ""}`}
-        onClick={toggleMenu}>
-        ☰
-      </nav>
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
 
-      <nav className={`nav${isOpen ? " active" : ""}`} id="nav">
-        <Link to="/">Home</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/about">About</Link>
-        <a href="https://t.me/@ArtemKhloptsev">Contact</a>
-      </nav>
-      <div className={`header_text${isOpen ? " open" : ""}`} id="header_text">
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
+  return (
+    <>
+      <div className="header">
         <div className="headerblock">
-          <Link to="/">Home</Link>{" "}
-        </div>
-        <div className="headerblock">
-          <Link to="/projects">Projects</Link>
-        </div>
-        <div className="headerblock">
-          <Link to="/about">About</Link>
-        </div>
-        <div className="headerblock">
-          <a href="https://t.me/@ArtemKhloptsev" target="_blank" rel="noreferrer">
-            Contact
+          <a href="Index.html">
+            <img
+              className="header_logo"
+              src="Images/sign-removebg-preview.png"
+              alt="Header_logo"
+            />
           </a>
         </div>
+        <nav
+          className={`hamburger ${isOpen ? "active" : ""}`}
+          onClick={toggleMenu}>
+          ☰
+        </nav>
+
+        <nav className={`nav${isOpen ? " active" : ""}`} id="nav">
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/about">About</Link>
+          <a href="https://t.me/@ArtemKhloptsev">Contact</a>
+        </nav>
+        <div className={`header_text${isOpen ? " open" : ""}`} id="header_text">
+          <div className="headerblock">
+            <Link to="/">Home</Link>{" "}
+          </div>
+          <div className="headerblock">
+            <Link to="/projects">Projects</Link>
+          </div>
+          <div className="headerblock">
+            <Link to="/about">About</Link>
+          </div>
+          <div className="headerblock">
+            <a
+              href="https://t.me/@ArtemKhloptsev"
+              target="_blank"
+              rel="noreferrer">
+              Contact
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+      {isOpen && <div className="menu-overlay" onClick={toggleMenu}></div>} ;
+      
+    </>
   );
 }
